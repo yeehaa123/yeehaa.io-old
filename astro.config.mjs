ee0cdc9c7d93507df26f78d1495d7e85eeedfbe9
Yeehaa <yeehaa@offcourse.io>
2023-08-17 01:24:03 +0200

    more migrate styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,7 @@ import { visit } from 'unist-util-visit';
 import section from "@hbsnow/rehype-sectionize";
 import unwrapImages from 'remark-unwrap-images';
 import svelte from "@astrojs/svelte";
-
-function slugify(str) {
-  return String(str).normalize('NFKD') // split accented characters into their base characters and diacritical marks
-  .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-  .trim() // trim leading or trailing whitespace
-  .toLowerCase() // convert to lowercase
-  .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
-  .replace(/\s+/g, '-') // replace spaces with hyphens
-  .replace(/20/g, '-').replace(/-+/g, '-'); // remove consecutive hyphens
-}
+import slugify from "../../utils/slugify.ts";
 
 function transformer(ast) {
   visit(ast, 'link', visitor);
